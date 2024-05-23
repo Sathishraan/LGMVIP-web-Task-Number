7887e02a67bf2d31e72d2fbf37b83dd143381283
Sathish P <146912497+Sathishraan@users.noreply.github.com>
2024-05-23 20:22:23 +0530

    Add files via upload

---
+++ user Card/script.js
@@ -0,0 +1,23 @@
+ function oneplus(){
+    window.location.href = 'https://www.oneplus.in/store';
+ }
+
+ function iphone(){
+    window.location.href = 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjNgaX4_qOGAxWwyxYFHdYmD8sYABAAGgJ0bA&ase=2&gclid=Cj0KCQjw0ruyBhDuARIsANSZ3wqGc1bWoAn3aKjV5rRhf8jl8wSN-83Qw2MYPBlmBWC6cIPOFDo5Dc8aAi2qEALw_wcB&ohost=www.google.com&cid=CAESVuD2LPGCDNQponfbFOYqBRktLEYZRWrLzirWa_v10bwtX5Vu8XQkiTQ4Q2WXZLB1TeXA4xkAhHKLU5RJayBvNQ0HXXbNr5BkQxufePlrOraV8VKxwy_I&sig=AOD64_3Hrbe6LIAvRo34zKh6jFz3TsJhIA&q&nis=4&adurl&ved=2ahUKEwj7wZ34_qOGAxVOYfUHHULFC2QQ0Qx6BAgQEAE';
+ }
+
+ function samsung(){
+    window.location.href = 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjFgvuG_6OGAxWcqGYCHacrDkAYABAAGgJzbQ&ase=2&gclid=Cj0KCQjw0ruyBhDuARIsANSZ3wqMi_jhzQrOV8q1qSNNcqL9wHUWX8bXK32IU9x3j0BQoQEINsUU9DkaAvArEALw_wcB&ohost=www.google.com&cid=CAESVuD2Fv0pjde2NuAPsmjQCiJSdTsGGzjsUt2t3ZXKf-he_n3cjULEI2oeIG3vdg6haLE2TkGaLgCuN0tW2CaX-9wV_k25ISVvlicMY2HZBpLOjDdn4VHY&sig=AOD64_02g6FBB0IjjTHsSE8VYsUXheePgg&q&nis=4&adurl&ved=2ahUKEwioq_KG_6OGAxVaamwGHR20BLwQ0Qx6BAgMEAE';
+ }
+
+ function realme(){
+    window.location.href = 'https://www.realme.com/in/';
+ }
+
+ function redme(){
+    window.location.href = 'https://www.mi.com/in/phone/';
+ }
+
+ function rog(){
+    window.location.href = 'https://rog.asus.com/phones/rog-phone-model/';
+ }
